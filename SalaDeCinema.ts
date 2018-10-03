11b8c895da456fd0a36ff751a7ac2a1fb475f8de
antoniokainan <42943952+antoniokainan@users.noreply.github.com>
2018-10-02 23:05:50 -0300

    Add files via upload

---
+++ SalaDeCinema.ts
@@ -0,0 +1,14 @@
+import {Cliente} from "./Cliente";
+
+class export SalaDeCinema{
+
+    private poltrona: number;
+    private cliente: Array<Cliente>;
+
+    public constructor(poltrona: number; cliente:Array<Cliente>){
+        this.poltrona = poltrona
+        this.cliente = []
+        for (i = 0; i < this.poltronas.lenght; i++) {
+            this.clientes.push()
+    }
+}
